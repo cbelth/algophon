061a9d23745ce654d15c06073248d680dc910ff0
cbelth <cbelth@umich.edu>
2024-05-17 13:04:54 -0600

    discrepancy

--- algophon/models/D2L/discrepancy.py
+++ algophon/models/D2L/discrepancy.py
@@ -5,30 +5,42 @@ class Discrepancy:
     A class for representing a discrepancy—i.e., a difference between URs and SRs.
     '''
 
-    def __init__(self, feature_diff) -> object:
+    def __init__(self, feature_diff: set) -> object:
         '''
+        :feature_diff: the features that differ between alternating ur_seg ~ sr_seg pairs
         '''
-        self.alternations = set()
-        self.annotated = list()
+        self.alternations = set() # stores the (ur_seg ~ sr_seg) alternations corresponding to the discrepancy
+        self.instances = list() # stores each instance of the discrepancy
         self.feature_diff = feature_diff
 
-    def __contains__(self, item):
+    def __contains__(self, item: tuple) -> bool:
         return item in self.alternations
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.feature_diff.__str__()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
     def tabulate(self, ur: SegStr, i: int, ur_seg: Seg, sr_seg: Seg) -> None:
         '''
-        '''
-        self.annotated.append((ur, i, sr_seg))
-        self.alternations.add((ur_seg, sr_seg))
+        :ur: the UR exibiting the discrepancy
+        :i: the index where the discrepancy occurs
+        :ur_seg:, :sr_seg: the ur_seg ~ sr_seg alternation
 
-    def get_alternating(self):
+        :return: None
+        '''
+        self.alternations.add((ur_seg, sr_seg)) # update alternations
+        self.instances.append((ur, i, sr_seg)) # update instances
+
+    def get_alternating(self) -> set:
+        '''
+        :return: a set of all the underling and surface Seg objects that are involved in the alternation
+        '''
         return set(it[0] for it in self.alternations).union(it[1] for it in self.alternations)
 
-    def get_alternating_urs(self):
+    def get_alternating_URs(self) -> set:
+        '''
+        :return: a set of all the underling Seg objects that are involved in the alternation
+        '''
         return set(it[0] for it in self.alternations)
