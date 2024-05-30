f105f87fa858b8c17141b2e00267670193d37100
cbelth <cbelth@umich.edu>
2024-05-30 12:07:30 -0600

    init tests

--- tests/tester.py
+++ tests/tester.py
@@ -7,6 +7,7 @@ from test_distance import TestDistance
 from test_utils import TestUtils
 from test_data_structures import TestDataStructures
 from test_d2l import TestD2L
+from test_miaseg import TestMiaseg
 
 '''
 A script to run all the test cases.
@@ -20,6 +21,7 @@ test_distance_suite = unittest.TestLoader().loadTestsFromTestCase(TestDistance)
 test_utils_suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
 test_datastructures_suite = unittest.TestLoader().loadTestsFromTestCase(TestDataStructures)
 test_d2l_suite = unittest.TestLoader().loadTestsFromTestCase(TestD2L)
+test_miaseg_suite = unittest.TestLoader().loadTestsFromTestCase(TestMiaseg)
 # combine the test suites
 suites = unittest.TestSuite([
     test_seg_suite,
@@ -29,7 +31,8 @@ suites = unittest.TestSuite([
     test_distance_suite,
     test_utils_suite,
     test_datastructures_suite,
-    test_d2l_suite
+    test_d2l_suite,
+    test_miaseg_suite
 ])
 # run the test suites
 unittest.TextTestRunner(verbosity=2).run(suites)
