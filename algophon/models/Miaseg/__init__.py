f6f46577bf6f52d879fcf056fe23a595fce3e8b7
cbelth <cbelth@umich.edu>
2024-05-30 13:11:56 -0600

    init paradigm

--- algophon/models/Miaseg/__init__.py
+++ algophon/models/Miaseg/__init__.py
@@ -1,2 +1,2 @@
-from algophon.models.Miaseg.miaseg import Miaseg
 from algophon.models.Miaseg.paradigm import Paradigm
+from algophon.models.Miaseg.miaseg import Miaseg
--- tests/test_miaseg.py
+++ tests/test_miaseg.py
@@ -5,6 +5,22 @@ sys.path.append('../')
 from algophon.models.Miaseg import Paradigm, Miaseg
 
 class TestMiaseg(unittest.TestCase):
+    def test_paradigm_init(self):
+        par = Paradigm('CAT')
+        assert(par is not None)
+        assert(par.root == 'CAT')
+        assert(len(par.words) == 0)
+
+    def test_paradigm_add_word(self):
+        par = Paradigm('CAT')
+        assert(len(par) == 0)
+        par.add_word(word='cat', features=())
+        assert(len(par) == 1)
+        par.add_word(word='cats', features=('pl',))
+        assert(len(par) == 2)
+        par.add_word(word='cat', features=()) # adding again changes nothing
+        assert(len(par) == 2)
+
     def test_miaseg_init(self):
         model = Miaseg()
         assert(model is not None)
