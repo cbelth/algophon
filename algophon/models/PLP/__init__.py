4f6f130848e44df685182c0ea6a98fa0842b9ef6
cbelth <cbelth@umich.edu>
2024-06-04 10:18:53 -0600

    init some PLP methods

--- algophon/models/PLP/__init__.py
+++ algophon/models/PLP/__init__.py
@@ -1,4 +1,4 @@
 from algophon.models.PLP.discrepancy import Discrepancy
-from algophon.models.PLP.rule import Rule
+from algophon.models.PLP.rule import PLPRule
 from algophon.models.PLP.grammar import Grammar
 from algophon.models.PLP.plp import PLP
